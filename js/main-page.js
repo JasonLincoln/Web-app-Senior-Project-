1c4c1baa0da6c17bb29cd5e9d7f9e306acddbc0a
Mason <maison.phonephomhom1@gmail.com>
2025-10-16 14:56:33 -0400

    added something

--- js/main-page.js
+++ js/main-page.js
@@ -1,5 +1,6 @@
 "use strict"
 
+// Slick JS
 $('.post-wrapper').slick({
     slidesToShow: 3,
     slidesToScroll: 2,
@@ -10,27 +11,40 @@ $('.post-wrapper').slick({
 
     responsive: [
     {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
+        breakpoint: 1200,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+        }
     },
     {
-      breakpoint: 1080,
-      settings: {
+        breakpoint: 1080,
+        settings: {
         slidesToShow: 2,
         slidesToScroll: 1
-      }
+        }
     },
     {
-      breakpoint: 720,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
+        breakpoint: 720,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
     }
     ]
 });
+
+// Hover JS
+
+let example = $(".skill-categories");
+let exampleText = $("#hiddenText1")
+
+example.mouseover(function() {
+    exampleText.css("display", "block");
+});
+
+example.mouseout(function() {
+    exampleText.css("display", "none");
+})
