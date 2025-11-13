b07a0783a17b060fc7c5f78f50416d98c617f16a
Xander Berger <xandergberger@gmail.com>
2025-11-13 14:26:33 -0500

    Continued work on admin page

---
+++ js/admin.js
@@ -0,0 +1,41 @@
+// Get All Users JS
+    
+
+// Get User By ID JS
+
+
+// Get All Skills JS
+
+
+// Get Skill By ID JS
+
+
+// Get All Achievements JS
+
+
+// Get Achievements By ID JS
+
+
+// Create Skill JS
+
+
+// Create Achievement JS
+
+
+// Update User By ID JS
+
+
+// Update Skill By ID JS
+
+
+// Update Achievement By ID JS
+
+
+// Delete User By ID
+
+
+// Delete Skill By ID
+
+
+// Delete Achievement By ID
+
