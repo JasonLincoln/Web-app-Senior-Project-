7254bbf4c4ab535b31b814e09f7ba682f749707e
Jason Lincoln <jaylincolnjr99@gmail.com>
2026-01-14 12:39:34 -0500

    test for redirect

--- Virtual-Contest-Project/Group-Project-Server/main.py
+++ Virtual-Contest-Project/Group-Project-Server/main.py
@@ -13,8 +13,8 @@ app = FastAPI()
 
 # Redirection to Login Page
 @app.get("/")
-def test(request: Request):
-    return RedirectResponse(url="/pages/login", status_code=status.HTTP_302_FOUND)
+def redirect_to_main(request: Request):
+    return RedirectResponse(url="/templates/login", status_code=status.HTTP_302_FOUND)
 
 #settings for env variables
 @app.get("/info")
