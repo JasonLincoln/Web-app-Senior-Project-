5115bd8a4703627ddc5ffcfda11d1155deba396c
unknown <xandergberger@gmail.com>
2026-01-15 13:59:17 -0500

    updated server

--- Virtual-Contest-Project/Group-Project-Server/routers/skills.py
+++ Virtual-Contest-Project/Group-Project-Server/routers/skills.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException, Path
+from fastapi import APIRouter, Depends, HTTPException, Path, Request
 from pydantic import BaseModel, Field
 from sqlalchemy import and_
 from sqlalchemy.orm import Session
